697f35c68af19e542761c371f63a1185223fdbcd
minhyeok0328 <alsgur7408@gmail.com>
2024-07-03 22:29:10 +0900

    chore: 기존 파일 제거
    다시 만들거임

--- preprocessing/src/main/java/org/crypto/Main.java
+++ preprocessing/src/main/java/org/crypto/Main.java
@@ -1,14 +1,10 @@
 package org.crypto;
 
-import org.crypto.processing.ByBit;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-
-        ByBit byBit = new ByBit();
-        byBit.start();
     }
 }
--- streaming/main.py
+++ streaming/main.py
@@ -9,15 +9,15 @@ async def main() -> None:
     container.wire(modules=[__name__])
 
     exchange_container: ExchangeContainer = container.exchange_container()
-    bybit = exchange_container.bybit()
-    upbit = exchange_container.upbit()
+    # bybit = exchange_container.bybit()
+    # upbit = exchange_container.upbit()
     binance = exchange_container.binance()
 
     async def run_crypto():
         await asyncio.gather(
             binance.run(),
-            bybit.run(),
-            upbit.run()
+            # bybit.run(),
+            # upbit.run()
         )
 
     await run_crypto()
